51f5378ba3cbd2b8660b38d9efc7e76f1b2c61d3
Morgan <morgan_demunck@yahoo.com>
2020-09-30 18:33:39 -0400

    initial commit

---
+++ New Unity Project/Assets/MusicController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+        }
+
+    }
+}
