7782ce7a79248d37e89adb2bf81c7031bc975f43
Morgan <morgan_demunck@yahoo.com>
2020-09-30 20:22:04 -0400

    Animation commit

---
+++ New Unity Project/Assets/Scripts/CatController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            anim.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            anim.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+
+        {
+
+            musicSource.loop = true;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+
+        {
+
+            musicSource.loop = false;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}
